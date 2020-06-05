a7afb600b596d0a157b3bb75ecceef84bd86a261
thiago-user <64050475+thiago-user@users.noreply.github.com>
2020-06-05 20:30:29 -0300

    Delete boot_time.py

--- boot_time.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-arq = open(r'D:\scripts\Python\algorithms\criados\boot_time.txt', 'a')
-arq.write('\n'+str(datetime.today()))
-arq.close()
-#mais uma linha
-#uma segunda linha
